0defe30329a9a3a0537266cd793b15c1f9a3e907
Chris Carpenter <mordocai@mordocai.net>
2011-05-25 15:31:11 -0500

    Example of using GET parameters.

--- project/settings.py
+++ project/settings.py
@@ -2,6 +2,7 @@
 URLS = (
     (r'^test/$', 'wsgifire.internal_views.list_request'),
     (r'^error-test/$', 'fake.fake.fake'),
+    (r'^$', 'project.views.add_numbers')
 )
 
 DEBUG = True
---
+++ project/views.py
@@ -0,0 +1,19 @@
+def add_numbers(request):
+    """Basic view that adds two numbers inputed by user."""
+    if (request.GET):
+        first = int(request.GET['number1'][0])
+        second = int(request.GET['number2'][0])
+        if first and second:
+            value = first + second
+            response_body = '<html>\n<head>\n\t<title>Numbers summed</title>\n</head>\n'
+            response_body += '<body>\n\t<h1>' + str(value) + '</h1>\n</body>\n</html>'
+        else:
+            response_body = '<html>\n<head>\n\t<title>Invalid Input</title>\n</head>\n'
+            response_body += '<body>\n\t<h1>Invalid Input</h1>\n</body>\n</html>'
+    else:
+        response_body = '<html>\n<head>\n\t<title>Add numbers</title>\n</head>\n'
+        response_body += '<body>\n\t<form method="get" action="">\n'
+        response_body += '\t\t<input name="number1" type="text" /><input name="number2" type="text" />\n'
+        response_body += '\t\t<input type="submit" />\n'
+        response_body += '\t</form>\n</body>\n</html>'
+    return response_body
